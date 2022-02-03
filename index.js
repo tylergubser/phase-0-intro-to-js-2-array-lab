ec8483d5c23512ade429f782ebe518d74029ca8c
tylergubser <tylergubser2200@gmail.com>
2022-02-03 17:26:44 -0600

    commit tree 429f390c373c0b060b8c8bf6130f3e32d88bbf77

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+}
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+}
+function destructivelyRemoveLastCat() {
+    return cats.pop(cats);
+
+}
+function destructivelyRemoveFirstCat() {
+    return cats.shift(cats);
+}
+function appendCat(name) {
+    const copyOfCats = [...cats];
+    copyOfCats.push(name);
+    return copyOfCats;
+    
+}
+function prependCat(name) {
+    const copyOfCats = [...cats];
+    copyOfCats.unshift(name);
+    return copyOfCats;
+}
+function removeLastCat() {
+    const copyOfCats = [...cats];
+    copyOfCats.pop();
+    return copyOfCats;
+}
+function removeFirstCat() {
+    const copyOfCats = [...cats];
+    copyOfCats.shift();
+    return copyOfCats;
+}
